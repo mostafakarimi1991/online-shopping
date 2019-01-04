72cbdb024a1a21b2dc06e80594fecd55077df241
MostafaKarimi <mostafa19.karimi91@gmail.com>
2019-01-04 15:58:07 +0330

    Project startup First commited on 20190104 at 04:00

---
+++ firstOnlineShoppingBackend/src/main/java/com/onlineShopping/firstOnlineShoppingBackend/App.java
@@ -0,0 +1,13 @@
+package com.onlineShopping.firstOnlineShoppingBackend;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
